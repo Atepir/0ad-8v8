701deedca3e7ae4a7fed3b11cefafae59c6951a9
Atepir <atepir0@gmail.com>
2024-12-12 20:50:40 +0100

    refactor: change playerSettingControlManagers initialization to static property

--- gui/gamesetup/Pages/GameSetupPage/GameSettings/GameSettingControlManager_8v8.js
+++ gui/gamesetup/Pages/GameSetupPage/GameSettings/GameSettingControlManager_8v8.js
@@ -1,4 +1,4 @@
-GameSettingControlManager.prototype.playerSettingControlManagers = Array.from(
+GameSettingControlManager.playerSettingControlManagers = Array.from(
     new Array(g_GreatMaxPlayerCount),
     (value, playerIndex) =>
         new PlayerSettingControlManager(playerIndex, setupWindow));
